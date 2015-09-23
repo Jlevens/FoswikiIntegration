a390548bba142ade1b719480822c958fd75c80fd
Julian Levens <julian@levens.eu>
2015-09-23 12:17:57 +0200

    Add analyseBuilds.pl

---
+++ analyseBuilds.pl
@@ -0,0 +1,58 @@
+#! /usr/bin/env perl
+#
+# FoswikiContinuousIntegrationContrib
+# Julian Levens
+# Copyright (C) 2015 ProjectContributors. All rights reserved.
+# ProjectContributors are listed in the AUTHORS file in the root of
+# the distribution.
+
+use File::FindLib qw( lib );
+use Setup;
+use ReadData;
+use Rules;
+
+use Path::Tiny;
+use Digest;
+
+for my $web ( keys %extWebRule ) {
+    next unless $web =~ m/^Extensions/;
+    
+    chdir("$scriptDir/$web");
+    my @Items = sort ( path(".")->children( qr/^!.*?(Contrib|Plugin|AddOn|Skin)\.tgz\z/ ) );
+    
+    for my $f ( @Items ) {
+        my $e = $f;
+        $e =~ s/^!//;
+        $e =~ s/\.tgz$//;
+        my $text = ''; #sprintf("%-40s", $e);
+
+        my $iter = path("$scriptDir/distro/$e/lib")->iterator( { recurse => 1 } );
+        while ( my $path = $iter->() ) {
+            my $base = $path->basename;
+            next unless $base eq 'MANIFEST';
+#            $text .= "    $path\n";
+            my @slurps = path("$path")->lines_raw( { chomp => 1 } );
+            for my $s (@slurps) {
+                next if $s =~ m/^\s*?(#|!)/;
+                next if $s =~ m/^\A\s*?\z/;
+                my ($mf) = split(' ', $s); # =~ m/^(.*?)\s*?.*?/;
+                $mf =~ s/^\"//;
+                $mf =~ s/\"$//;
+                next if $mf =~ m{^(lib/CPAN|pub/|working/|test/|solr/|locale/)};
+                next if $mf !~ m{^(lib/|data/)};
+
+                if( -e "$scriptDir/distro/$e/$mf" ) {
+                say "!! $mf" if !-e "$scriptDir/distro/$e/$mf";            
+                say path("$scriptDir/distro/$e/$mf")->digest("MD5") if -e "$scriptDir/distro/$e/$mf";
+                say "-- $mf" if !-e "$scriptDir/Extensions/!$e\.tgz/$mf";            
+                say path("$scriptDir/Extensions/!$e\.tgz/$mf")->digest("MD5") if -e "$scriptDir/Extensions/!$e\.tgz/$mf";
+#                $text .= "$mf\n" if defined $mf;
+#                say "!!$s" if !defined $mf;
+            }
+            last;
+        }
+#        print $text;
+    }
+}
+
+exit 0;
