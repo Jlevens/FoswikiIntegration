c12034a0b0102d1f547cff8c30cc2300fb4ace9f
Julian Levens <julian@levens.eu>
2015-09-24 14:03:33 +0200

    Add all.sh'

---
+++ all.sh
@@ -0,0 +1,12 @@
+
+perl syncAll.pl
+perl getTopics.pl
+perl analyseForms.pl
+perl analyseForms.pl
+perl analyseItems.pl
+perl analyseDigests.pl;
+perl analyseInstallers.pl
+perl mergeJson.pl
+perl showSummary.pl
+perl analyseAll.pl
+
