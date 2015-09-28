dd4bfe93feb858953a35f07181cf7b3edbfcd29a
Julian Levens <julian@levens.eu>
2015-09-28 13:49:50 +0200

    Any Extensions with no lib?

--- analyseRepoNames.pl
+++ analyseRepoNames.pl
@@ -31,6 +31,7 @@ while ( my $path = $iter->() ) {
 
 say "\nHere's the list\n";
 
+my @nolib;
 for my $parent ( sort keys %items ) {
     for my $base ( sort keys $items{ $parent } ) {
         printf "%-40s %s\n", $base, $parent if $items{ $parent }{ $base } > 1;
@@ -38,6 +39,10 @@ for my $parent ( sort keys %items ) {
     next unless $parent =~ m{$scriptDir/distro/[^/]*?\z};
     
     printf "%d %-38s %s\n", $items{ $parent }{ lib } // 0, 'lib', $parent unless $items{ $parent }{ lib };
+    push @nolib, $parent;
 }
+
+$, = " ";
+say @nolib;
 
 exit 0;
