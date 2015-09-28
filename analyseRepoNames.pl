22cca519d7f5ad0eac83b1c804131ed9dd85c77f
Julian Levens <julian@levens.eu>
2015-09-28 11:38:17 +0200

    Any Extensions with no lib?

--- analyseRepoNames.pl
+++ analyseRepoNames.pl
@@ -37,7 +37,7 @@ for my $parent ( sort keys %items ) {
     }
     next unless $parent =~ m{$scriptDir/distro/[^/]*?\z};
     
-    printf "%d %-38s %s\n", $items{ $parent }{ lib } // 0, 'lib', $parent;
+    printf "%d %-38s %s\n", $items{ $parent }{ lib } // 0, 'lib', $parent unless $items{ $parent }{ lib };
 }
 
 exit 0;
