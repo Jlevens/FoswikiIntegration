20719900084105cc90f930b97b29aa32b5468917
Julian Levens <julian@levens.eu>
2016-09-11 18:54:31 +0200

    Format output as tables to be placed on f.o.

--- deps.pl
+++ deps.pl
@@ -12,11 +12,25 @@ use Setup;
 use MetaCPAN::Client;
 use Path::Tiny;
 use Module::CoreList;
+use Path::Iterator::Rule;
 
 chdir($scriptDir);
 
-my $dinfo = path('repos')->visit( \&checkNode, { recurse => 1 } );
-$Data::Dumper::Indent = 1;
+my $rule = Path::Iterator::Rule->new; # match anything
+$rule->skip_dirs(".git")->file->name('DEPENDENCIES'); # OK
+
+# iterator interface
+my $next = $rule->iter( 'distro' );
+my $dinfo = {};
+my $mcpan  = MetaCPAN::Client->new();
+
+while ( defined( my $file = $next->() ) ) {
+    checkNode( $file, $dinfo );
+}
+
+#my $dinfo = path('distro')->visit( \&checkNode, { recurse => 1 } );
+
+#$Data::Dumper::Indent = 1;
 #print Data::Dumper->Dump( [ \$dinfo ], [ 'DEPENDENCIES' ] );
 
 my @types = sort keys %{$dinfo->{deps}};
@@ -29,15 +43,15 @@ for my $type (@types) {
         for my $ext (@exts) {
             my ($otype, $odname) = @{$dinfo->{deps}{$type}{$dname}{$ext}};
 
-            my $change = ($type ne $otype ? 'T' : ' ') . ($dname ne $odname ? 'N' : ' ');
-            my $info = sprintf "%-33s %-s", $ext, join(' | ', $otype, $odname ); # @{$dinfo->{deps}{$type}{$dname}{$ext}} );
+            my $change = ($type ne $otype ? 'T' : '') . ($dname ne $odname ? 'N' : '');
+            my $info = sprintf "%s | %s", $ext, join(' | ', $otype, $odname ); # @{$dinfo->{deps}{$type}{$dname}{$ext}} );
                             
             push @changes,
-                sprintf "$change %-9s %-43s $info",  $type, $dname  if $change ne '  ';
-            printf     ("$change %-9s %-43s $info\n", $ptype, $pdname);
-            ($ptype, $pdname) = ('', '');
+                sprintf "| $change | %s | %s | $info |",  $type, $dname  if $change ne '';
+            printf     ("| $change | %s | %s | $info |\n", $ptype, $pdname);
+            ($ptype, $pdname) = ('^', '^');
         }
-        print "\n";
+#        print "\n";
     }
 }
 $" = "\n";
@@ -48,12 +62,10 @@ exit 0;
 sub checkNode {
     my ( $node, $data ) = @_;
 
-    my @dirs = File::Spec->splitdir( $node->stringify );
+    my @dirs = File::Spec->splitdir( $node );
     my $depth = (scalar @dirs) - 1;
     my $leaf  = $dirs[$depth];
     my $ext = $dirs[ $depth - 1 ];
-
-    my $mcpan  = MetaCPAN::Client->new();
 
     my %types = (
         external => 'external',
@@ -65,57 +77,55 @@ sub checkNode {
         unknown => 'unknown',
     );
 
-    if( -f $node && $leaf eq 'DEPENDENCIES' ) {
-        my @deps = -e $node ? $node->lines( { chomp => 1 } ) : ();
-        my $only = '';
-        for my $dep (@deps) {
-            next unless $dep;
-            next if substr($dep,0,1) eq '#';
-            chomp($dep);
-            if( $dep =~ m/^ONLYIF/ ) {
-                $only = $dep;
-                next;
-            }
-            #print "<<$dep>>\n";
-            my ($odname, $select, $otype, $desc) = split(',', $dep);
-
-            $otype  = 'unknown' if !$otype;
-            $otype =~ s/\s//g;
-            $otype = lc($otype);
-            my $type = $otype;
-            $type = $types{$type};     
-           
-            my $dname = $odname;
-
-            $dname = 'unknown' if !$dname;
-            $select = 'unknown' if !$select;
-            $type  = 'unknown' if !$type;
-            $desc  = 'No-desc' if !$desc;
-
-            my ($cf, $cff);
-            eval {
-                $cf = $mcpan->module($dname, { fields => "distribution,version" } );
-            };
-            if( !$@ ) { # Found in cpan, so it's a CPAN module and the name is already good
-                $type = 'cpan'; # Or at least a module has been found on CPAN = $dname
-                $cff = sprintf("%-30s %-10s", $cf->{data}{distribution}, $cf->{data}{version} );
-            }
-            else { # Not found in cpan, so is it perl and is the name good?
-                $cff = '';
-
-                if( $dname !~ m/::/ ) {
-                   if(    $dname =~ m/Contrib$/ ) { $dname = "Foswiki::Contrib::$dname"; }
-                   elsif( $dname =~ m/Plugin$/ ) { $dname = "Foswiki::Plugins::$dname"; }
-                }
-
-                $type = 'perl' if $dname =~ m/^(Foswiki::|TWiki::)/;                            
-            }            
-
-            if( $dname =~ m/(ImageMagick|wvHTML)/ ) {
-                $type = 'external';
-            }
-            
-            $data->{deps}{$type}{$dname}{$ext} = [ $otype, $odname, $select, $only, $desc, $cff, (Module::CoreList::is_core($dname) ? 'CoreList' : '') ];
+    my @deps = -e $node ? path($node)->lines( { chomp => 1 } ) : ();
+    my $only = '';
+    for my $dep (@deps) {
+        next unless $dep;
+        next if substr($dep,0,1) eq '#';
+        chomp($dep);
+        if( $dep =~ m/^ONLYIF/ ) {
+            $only = $dep;
+            next;
         }
+#        print "<<$dep>>\n";
+        my ($odname, $select, $otype, $desc) = split(',', $dep);
+
+        $otype  = 'unknown' if !$otype;
+        $otype =~ s/\s//g;
+        $otype = lc($otype);
+        my $type = $otype;
+        $type = $types{$type};     
+       
+        my $dname = $odname;
+
+        $dname = 'unknown' if !$dname;
+        $select = 'unknown' if !$select;
+        $type  = 'unknown' if !$type;
+        $desc  = 'No-desc' if !$desc;
+
+        my ($cf, $cff);
+        eval {
+            $cf = $mcpan->module($dname, { fields => "distribution,version" } );
+        };
+        if( !$@ ) { # Found in cpan, so it's a CPAN module and the name is already good
+            $type = 'cpan'; # Or at least a module has been found on CPAN = $dname
+            $cff = sprintf("%-30s %-10s", $cf->{data}{distribution}, $cf->{data}{version} );
+        }
+        else { # Not found in cpan, so is it perl and is the name good?
+            $cff = '';
+
+            if( $dname !~ m/::/ ) {
+               if(    $dname =~ m/Contrib$/ ) { $dname = "Foswiki::Contrib::$dname"; }
+               elsif( $dname =~ m/Plugin$/ ) { $dname = "Foswiki::Plugins::$dname"; }
+            }
+
+            $type = 'perl' if $dname =~ m/^(Foswiki::|TWiki::)/;                            
+        }            
+
+        if( $dname =~ m/(ImageMagick|wvHTML)/ ) {
+            $type = 'external';
+        }
+        
+        $data->{deps}{$type}{$dname}{$ext} = [ $otype, $odname, $select, $only, $desc, $cff, (Module::CoreList::is_core($dname) ? 'CoreList' : '') ];
     }
 }
